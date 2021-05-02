6870e4fbb55a60332cd109f7a4c98beed54767ad
geeky-bawa <74357340+geeky-bawa@users.noreply.github.com>
2021-05-03 00:03:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-let positionStart = { 1: -80 };
+
 let loadImage = (src, callback) => {
     let img = document.createElement("img");
     img.onload = () => callback(img);
@@ -13,24 +13,21 @@ let frames = {
     idle: [1, 2, 3, 4, 5, 6, 7, 8],
     kick: [1, 2, 3, 4, 5, 6, 7],
     punch: [1, 2, 3, 4, 5, 6, 7],
-    forward: [1, 2, 3, 4, 5, 6],
-    backward: [1, 2, 3, 4, 5, 6],
 };
 
 let loadImages = (callback) => {
-    let images = { idle: [], kick: [], punch: [], forward: [], backward: [] };
+    let images = { idle: [], kick: [], punch: [] };
     let imagesToLoad = 0;
 
-    ["idle", "kick", "punch", "forward", "backward"].forEach((animation) => {
+    ["idle", "kick", "punch"].forEach((animation) => {
         let animationFrames = frames[animation];
-        imagesToLoad += animationFrames.length;
-
+        imagesToLoad = imagesToLoad + animationFrames.length;
         animationFrames.forEach((frameNumber) => {
             let path = imagePath(frameNumber, animation);
 
             loadImage(path, (image) => {
                 images[animation][frameNumber - 1] = image;
-                imagesToLoad -= 1;
+                imagesToLoad = imagesToLoad - 1;
                 if (imagesToLoad === 0) {
                     callback(images);
                 }
@@ -39,18 +36,10 @@ let loadImages = (callback) => {
     });
 };
 let animate = (ctx, images, animation, callback) => {
-    if (animation == "backward" && positionStart[1] > -80) {
-        positionStart[1] -= 40;
-    } else if (animation == "forward" && positionStart[1] < 360) {
-        positionStart[player] += 40;
-    }
-
-    console.log(positionStart);
-
     images[animation].forEach((image, index) => {
         setTimeout(() => {
-            ctx.clearRect(positionStart[1], 0, 500, 500);
-            ctx.drawImage(image, positionStart[1], 0, 500, 500);
+            ctx.clearRect(0, 0, 500, 500);
+            ctx.drawImage(image, 0, 0, 500, 500);
         }, index * 100);
     });
     setTimeout(callback, images[animation].length * 100);
@@ -79,13 +68,6 @@ loadImages((images) => {
         queuedAnimations.push("punch");
     };
 
-    document.getElementById("forward").onclick = () => {
-        queueAnimation.push("forward");
-    };
-    document.getElementById("backward").onclick = () => {
-        queueAnimation.push("backward");
-    };
-
     document.addEventListener("keyup", (event) => {
         const key = event.key;
 
@@ -93,10 +75,6 @@ loadImages((images) => {
             queuedAnimations.push("kick");
         } else if (key === "ArrowDown") {
             queuedAnimations.push("punch");
-        } else if (key === "ArrowRight") {
-            queueAnimation.push("forward");
-        } else if (key === "ArrowLeft") {
-            queueAnimation.push("backward");
         }
     });
 });
